8a2da29c21935f5d7c38f41abb03049f18cc820d
brillianto <brillianto.pamungkas@next-ti.co.id>
2025-02-28 11:02:28 +0700

    first commit

---
+++ src/main/java/com/arnog/burnog/BurnogApplication.java
@@ -0,0 +1,13 @@
+package com.arnog.burnog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurnogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurnogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arnog/burnog/BurnogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnog.burnog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurnogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
